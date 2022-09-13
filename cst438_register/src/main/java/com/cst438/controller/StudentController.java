564501aa5858a6169314d40f01550422d544e166
mold <mold@RemoteMint20.3LizardSpider>
2022-09-12 17:46:05 -0700

    Added Student Controller ....2 (did it happen the first time? probably not)

--- cst438_register/src/main/java/com/cst438/controller/StudentController.java
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -1,5 +1,12 @@
 package com.cst438.controller;
 
-public class StudentController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class StudentController {
+	@PostMapping("/student")
+	public String createNewStudent() {
+		return "student id = 12398";
+	}
 }
