576c96b5ee54237e43b0f07b3872f05327a9ad2a
arlon <arlon@arlon.arlon>
2022-09-13 12:58:46 -0700

    It's finally adding to the db - params hardcoded...

--- cst438_register/src/main/java/com/cst438/controller/StudentController.java
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -3,37 +3,54 @@ package com.cst438.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.domain.Course;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.ScheduleDTO;
 import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
 
 @RestController
 public class StudentController {
 	
-	@PostMapping("/student")	
-	public String createNewStudent() {
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@PostMapping("/student")
+	@Transactional
+	public Student createNewStudent( /* @RequestParam("name") String name*/) {
+
+		String name = "arlon2"; // student's email
+		String student_email = "test@csumb.edu"; // student's email
+		int student_status_code = 0;
 		
-		return "student id = 12398";
+		Student s=new Student();
+		s.setName(name);
+		s.setEmail(student_email);
+		s.setStatusCode(student_status_code);
+		
+		//if (student!= null && course!=null && student.getStatusCode()==0) {
+
+		Student student=studentRepository.save(s);
+		
+		return student;
+		//ok this is super wierd I've changed stuff and for some reason can't push.
+		//this has had super progress: user arlon1 goes into the db here.
+		//in other words this does change the db but the info is hardcoded here,
+		//request parameters don't work yet. You can see they're there but...
+		//I'm doing something wrong with the parameters being sent in
+		//but this is a lot farther than I had it yesterday because the db is changed!
+		// I see what I was doing there's a passphrase AND a passWORD for ssh...
+		// No, I can't get it to push...
 	}
 	
-	//copied from schedule controller:
-	/* 
-	 * helper method to transform course, enrollment, student entities into 
-	 * a an instance of ScheduleDTO to return to front end.
-	 * This makes the front end less dependent on the details of the database.
-	 */
-	/*
-	 * private ScheduleDTO createSchedule(int year, String semester, Student s,
-	 * List<Enrollment> enrollments) { ScheduleDTO result = new ScheduleDTO();
-	 * result.semester = semester; result.year = year; result.student_email =
-	 * s.getEmail(); result.student_id = s.getStudent_id();
-	 * ArrayList<ScheduleDTO.CourseDTO> courses = new ArrayList<>();
-	 * 
-	 * for (Enrollment e : enrollments) { ScheduleDTO.CourseDTO courseDTO =
-	 * createCourseDTO(e); courses.add(courseDTO); } result.courses = courses;
-	 * return result; }
-	 */
 }
