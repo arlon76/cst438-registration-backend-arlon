ff062e407366fecd05b74aad128de8cba795d918
arlon <arlon@RemoteMint20.3LizardSpider>
2022-09-13 23:12:59 -0700

    Update works and add uses the DTO instead of the Student directly

--- cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
@@ -8,9 +8,42 @@ package com.cst438.domain;
  *
  */
 public class StudentDTO{
-	public int student_id;
-	public String name;
-	public String email;
-	public int statusCode;
-	public String status;
+	
+	private int student_id;
+	private String name;
+	private String email;
+	private int statusCode;
+	private String status;
+	
+	public int getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
