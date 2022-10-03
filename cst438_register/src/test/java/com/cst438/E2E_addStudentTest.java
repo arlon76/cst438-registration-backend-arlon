6c98292a338e8776303e09ceab191edd31d96aef
arlon76 <arlon_76@yahoo.com>
2022-10-02 18:57:07 -0700

    3 e2e tests completed, add student, can't add because an email exists, can't because statusCode isn't an integer, then fix, add

--- cst438_register/src/test/java/com/cst438/E2E_addStudentTest.java
+++ cst438_register/src/test/java/com/cst438/E2E_addStudentTest.java
@@ -63,8 +63,9 @@ public class E2E_addStudentTest {
 			we.click();
 			Thread.sleep(SLEEP_DURATION);
 			
+			String email_to_add="fido@fidosfood"+Math.pow(10,8)*Math.random()+".com";
 			driver.findElement(By.id("new_student_name_entry_field")).sendKeys("fido");
-			driver.findElement(By.id("new_student_email_entry_field")).sendKeys("fido@fidosfood.com");
+			driver.findElement(By.id("new_student_email_entry_field")).sendKeys(email_to_add);/*100M emails*/
 			driver.findElement(By.id("new_student_status_entry_field")).sendKeys("hungry");
 			driver.findElement(By.id("new_student_statusCode_entry_field")).sendKeys("1000");
 			// find and click the submit-student-button button
@@ -73,7 +74,99 @@ public class E2E_addStudentTest {
 			Thread.sleep(SLEEP_DURATION);
 			
 			Thread.sleep(SLEEP_DURATION);
- /*
+			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 78 e2e add student looking for toast...");
+			String toast_text = driver.findElement(By.cssSelector(".Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 80 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"Student successfully added");
+			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 78 e2e add student looking for toast...");
+			toast_text = driver.findElement(By.cssSelector(".Toastify__toast:nth-child(2) .Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 80 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"DB row successfully added in student table.");
+			
+			//that's it I think, a round trip end-to-end add student test. Oh, wait maybe a failed email-repeats test:		
+			Thread.sleep(SLEEP_DURATION);
+			we = driver.findElement(By.id("add-student-button"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			driver.findElement(By.id("new_student_name_entry_field")).sendKeys("fido");
+			driver.findElement(By.id("new_student_email_entry_field")).sendKeys(email_to_add/*repeats above email*/);/*100M emails*/
+			driver.findElement(By.id("new_student_status_entry_field")).sendKeys("hungry");
+			driver.findElement(By.id("new_student_statusCode_entry_field")).sendKeys("1000");
+			// find and click the submit-student-button button
+			we = driver.findElement(By.id("submit-student-button"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);			
+			Thread.sleep(SLEEP_DURATION);
+			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 109 e2e add student looking for toast...");
+			toast_text = driver.findElement(By.cssSelector(".Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 111 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"Error when adding");
+			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 116 e2e add student looking for toast...");
+			toast_text = driver.findElement(By.cssSelector(".Toastify__toast:nth-child(2) .Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 118 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"DB row successfully added in student table.");//this is a different bug - why does it say this?
+			
+			//check for invalid integer:
+			Thread.sleep(SLEEP_DURATION);
+			we = driver.findElement(By.id("add-student-button"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);
+			
+			driver.findElement(By.id("new_student_name_entry_field")).sendKeys("fido");
+			driver.findElement(By.id("new_student_email_entry_field")).sendKeys(email_to_add/*repeats above email*/);/*100M emails*/
+			driver.findElement(By.id("new_student_status_entry_field")).sendKeys("hungry");
+			driver.findElement(By.id("new_student_statusCode_entry_field")).sendKeys("1000a");
+			// find and click the submit-student-button button
+			we = driver.findElement(By.id("submit-student-button"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 109 e2e add student looking for toast...");
+			toast_text = driver.findElement(By.cssSelector(".Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 111 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"The status code needs to be an integer");
+			
+			//backspace it and try again
+			driver.findElement(By.id("new_student_statusCode_entry_field")).clear();
+			driver.findElement(By.id("new_student_statusCode_entry_field")).sendKeys("1001");
+			email_to_add="fido@fidosfood"+Math.pow(10,8)*Math.random()+".com";//don't forget to switch the email again
+			driver.findElement(By.id("new_student_email_entry_field")).sendKeys(email_to_add/*repeats above email*/);/*100M emails*/
+
+			we = driver.findElement(By.id("submit-student-button"));
+			we.click();
+			Thread.sleep(SLEEP_DURATION);			
+			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 78 e2e add student looking for toast..."); //2nd and 3rd childs because 'invalid integer' is still  out
+			toast_text = driver.findElement(By.cssSelector(".Toastify__toast:nth-child(2) .Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 80 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"Student successfully added");
+			
+			//.Toastify__toast-body div:nth-child(2)
+			System.out.println("Line 78 e2e add student looking for toast...");
+			toast_text = driver.findElement(By.cssSelector(".Toastify__toast:nth-child(3) .Toastify__toast-body div:nth-child(2)")).getText();
+			System.out.println("Line 80 e2e add student possibly found toast..."+toast_text);
+			
+			assertEquals(toast_text,"DB row successfully added in student table.");
+			
+			//that's three or four tests, add student, can't add student because email exists, can't because statusCode!=integer, fix=>can
+			
+ /* More example code from the exercise:
 			// get the 2 multiply factors
 			String factora = driver.findElement(By.id("factora")).getText();
 			String factorb = driver.findElement(By.id("factorb")).getText();
@@ -118,7 +211,8 @@ public class E2E_addStudentTest {
 			assertEquals(factora, we_list.get(0).getText());
 			assertEquals(factorb, we_list.get(1).getText());
 			assertEquals("true", we_list.get(4).getText());
-			*/
+		*/
+		
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			throw ex;
